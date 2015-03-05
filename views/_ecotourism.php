3473be90c532cbc157a93053ca2969c2ffcd8dfa
rdechamps <romain@inedits.com>
2015-03-05 18:33:14 +0100

    Ajout des pages

--- views/_ecotourism.php
+++ views/_ecotourism.php
@@ -1,19 +1,25 @@
 <?php require("_head.php"); ?>
 <?php require("_header.php"); ?>
 
-<div class="main-container">
-	<div class="teaser">
+<div class="main-container col col-12">
+	<div class="teaser ecotourism">
 
 	</div>
 	<div class="content">
-		<div class="left-side">
+        <h1><?php echo $content['home_ecotourism_tilte'];  ?></h1>
+		<div class="left-side col col-6 home">
 
+           <?php echo $content['home_ecotourism_txt'];  ?>
 		</div>
-		<div class="right-side">
-
+		<div class="right-side col col-6">
+<?php echo $content['home_ecotourism_txt_right'];  ?>
 		</div>
+	</div>
 	</div>
 
 </div>
 
 <?php require("_footer.php"); ?>
+
+
+
